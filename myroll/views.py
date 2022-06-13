447c8eacf7a1504fb62965e49788e8e2662eca79
FishHart <icepekka@icloud.com>
2022-06-13 16:18:28 +0900

    login, signup を実装

--- myroll/views.py
+++ myroll/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def myroll(request):
     for_range10 = [i for i in range(10)]
 
     return render(request, 'myroll/index.html', {'for_range10': for_range10})
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('myroll')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from myroll import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myroll.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/', views.signup, name='default_signup'),
 ]
