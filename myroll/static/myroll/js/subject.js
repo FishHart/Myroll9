c94dd052d7f9ecd82df28d001ec7135877a868a9
FishHart <icepekka@icloud.com>
2022-07-12 16:24:56 +0900

    押された教科によってモーダルウィンドウの表示を変える途中

--- myroll/static/myroll/js/subject.js
+++ myroll/static/myroll/js/subject.js
@@ -3,24 +3,27 @@ const buttonOpen = document.getElementById('modalOpen');
 const modal = document.getElementById('easyModal');
 const buttonClose = document.getElementsByClassName('modalClose')[0];
 
+// const subject = getElementById('subject');
+// const subName = getElementById('subName');
+
 // ボタンがクリックされた時
 buttonOpen.addEventListener('click', modalOpen);
 function modalOpen() {
-  modal.style.display = 'block';
-  body.style.backgroundColor = 'black';
+    modal.style.display = 'block';
+    body.style.backgroundColor = 'black';
 }
 
 // バツ印がクリックされた時
 buttonClose.addEventListener('click', modalClose);
 function modalClose() {
-  modal.style.display = 'none';
-  body.style.backgroundColor = 'white';
+    modal.style.display = 'none';
+    body.style.backgroundColor = 'white';
 }
 
 // モーダルコンテンツ以外がクリックされた時
 addEventListener('click', outsideClose);
 function outsideClose(e) {
-  if (e.target == modal) {
-    modal.style.display = 'none';
-  }
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
 }
