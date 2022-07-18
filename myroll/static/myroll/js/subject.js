ff2fe5c9358e3b4d637ce55fb6be65521357b1b5
FishHart <icepekka@icloud.com>
2022-07-18 12:38:27 +0900

    モーダルウィンドウ表示のボタンを科目一覧それぞれにつけた。

--- myroll/static/myroll/js/subject.js
+++ myroll/static/myroll/js/subject.js
@@ -1,17 +1,38 @@
 const body = document.querySelector('html body');
-const buttonOpen = document.getElementById('modalOpen');
+// const buttonOpen = document.getElementById('modalOpen');
+const buttonOpen = document.getElementsByClassName('button');
+const subjectName = document.getElementsByClassName('subjectName');
+
 const modal = document.getElementById('easyModal');
 const buttonClose = document.getElementsByClassName('modalClose')[0];
 
 // const subject = getElementById('subject');
-// const subName = getElementById('subName');
+const subName = document.getElementById('subName');
 
 // ボタンがクリックされた時
-buttonOpen.addEventListener('click', modalOpen);
-function modalOpen() {
-    modal.style.display = 'block';
-    body.style.backgroundColor = 'black';
+// buttonOpen.addEventListener('click', modalOpen);
+// function modalOpen() {
+//     modal.style.display = 'block';
+//     body.style.backgroundColor = 'black';
+// }
+
+for(let i = 0; i < buttonOpen.length; i++) {
+    function modalOpen() {
+        modal.style.top = document.documentElement.scrollTop + 'px';
+        modal.style.display = 'block';
+        console.log(modal.style.top);
+        console.log(document.documentElement.scrollTop);
+        body.style.backgroundColor = 'black';
+        subName.innerText = subjectName[i].textContent;
+    }
+    buttonOpen[i].addEventListener('click', modalOpen);
+    // buttonOpen[i].onclick = function() {
+    //     modal.style.display = 'block';
+    //     // body.style.backgroundColor = 'black';
+    //     // subName.innerText = subjectName[i];
+    // }
 }
+
 
 // バツ印がクリックされた時
 buttonClose.addEventListener('click', modalClose);
