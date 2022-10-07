0861f7b3ed612c58ad08ea2fb6802224de73f285
FishHart <icepekka@icloud.com>
2022-10-07 16:37:37 +0900

    myrollにSubjectモデルを追加 migrateを行った

--- myroll/models.py
+++ myroll/models.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Subject(models.Model):
+    name = models.CharField(max_length=10, primary_key=True)
+    total_number = models.IntegerField(default=0)
+    count = models.IntegerField(default=0)
+    charge = models.CharField(max_length=10)
+
+    def assignment(self):
+        self.total_number += 1
+        self.save()
+
+    def __str__(self):
+        return self.name
