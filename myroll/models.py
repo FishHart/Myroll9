f28cf410b4e6496d8741de2a1622c9f21fc0a926
FishHart <icepekka@icloud.com>
2022-10-27 18:42:26 +0900

    Attendingを追加、Subjectを変更

--- myroll/models.py
+++ myroll/models.py
@@ -1,17 +1,22 @@
+from unittest.util import _MAX_LENGTH
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
 
-class Subject(models.Model):
-    name = models.CharField(max_length=10, primary_key=True)
-    total_number = models.IntegerField(default=0)
-    count = models.IntegerField(default=0)
-    charge = models.CharField(max_length=10)
+class Attending(models.Model):
+    userpk = models.IntegerField(unique=True, primary_key=True)
+    subject = models.CharField(max_length=10)
 
-    def assignment(self):
-        self.total_number += 1
-        self.save()
+    # def assignment(self):
+    #     self.total_number += 1
+    #     self.save()
 
     def __str__(self):
-        return self.name
+        return self.subject
+
+class Subject(models.Model):
+    name = models.CharField(max_length=10, primary_key=True)
+    total = models.IntegerField()
+    count = models.IntegerField()
+    
